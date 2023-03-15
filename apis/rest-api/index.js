d8d9a1b81650c0aa0fdaa15c7d1c6a575841f20a
kenneth-alt <kenneth.ugo15@gmail.com>
2023-03-15 13:07:06 -0600

    update index.js

--- apis/rest-api/index.js
+++ apis/rest-api/index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 
 const app = express()
-const port = 3030
+const port = process.env.PORT || 3030
 
 app.get('/', (req, res) => {
     res.send('Server is up...')
@@ -11,6 +11,5 @@ app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3])
 })
 
-app.listen(port, () => {
-    console.log(`listening on port ${port}`)
-})
+app.listen(port, () => console.log(`listening on port ${port}`))
+
