b0a6d0aa2e46be47536b32e6eb28c98f314e379a
kenneth-alt <kenneth.ugo15@gmail.com>
2023-03-15 16:04:55 -0600

    add schema to index.js

--- apis/node-fetch/index.js
+++ apis/node-fetch/index.js
@@ -3,14 +3,27 @@ import dotenv from "dotenv";
 
 dotenv .config();
 
-console.log(process.env.MONGO_URL)
+// console.log(process.env.MONGO_URL)
 
-const connectionString = process.env.MONGO_URL
+const connectionString = process.env.MONGO_URL;
 
-async function main() {
+const db = await mongoose.connect(connectionString);
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+});
+
+const User = mongoose.model("user", userSchema);
+
+const users = await User.find();
+
+console.log(users);
+
+/* async function main() {
   await mongoose.connect(connectionString);
 }
 
 main()
 .then(() => console.log("connected"))
-.catch((err) => console.log(err));
+.catch((err) => console.log(err)); */
