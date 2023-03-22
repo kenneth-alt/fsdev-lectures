eac42ebf7ace882218f29c287648b01022a6ca60
kenneth-alt <kenneth.ugo15@gmail.com>
2023-03-22 15:48:16 -0600

    modify server.js

--- apis/anatomy-of-http/server.js
+++ apis/anatomy-of-http/server.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const app = express();
 
+app.use(express())
+app.use(express.urlencoded())
+
 const PORT = process.env.PORT || 4444;
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
+
+app.get('/*', (req, res) => {
+    console.log('Path:', req.path);
+    console.log('Query:', req.query);
+    console.log('Body:', req.body);
+    res.send("Hello home!");
+  });
+
+app.post('/pa*', (req, res) => {
+    console.log('Path:', req.path);
+    console.log('Query:', req.query);
+    console.log('Body:', req.body);
+    res.send('Posted successfully');
+})
