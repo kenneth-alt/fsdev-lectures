4e30c50252a7698fe09e0fc5911643ccde57799d
kenneth-alt <kenneth.ugo15@gmail.com>
2023-04-14 12:03:59 -0600

    it's working

--- react/react-with-chris/server/server.js
+++ react/react-with-chris/server/server.js
@@ -1,5 +1,8 @@
 import express from 'express'
+import cors from 'cors'
 const app = express()
+app.use(cors())
+
 const port = process.env.PORT || 5051
 
 const moves = [
--- react/react-with-chris/webpage/src/App.jsx
+++ react/react-with-chris/webpage/src/App.jsx
@@ -3,29 +3,70 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const computerOpponentUrl = 'http://localhost:5051/rps'
+
 function Button(props){
   const {text, fire} = props
-  return <button style={{backgroundColor: 'white', color: 'black', margin: 4}} onClick={fire}>{text}</button>
+  return <button style={{backgroundColor: 'white', color: 'black', margin: 4}} 
+  onClick={fire}>{text}</button>
 }
 
 function App() {
   const [myGuess, setMyGuess] = useState('No guess yet') 
+  const [computerGuess, setComputerGuess] = useState('No guess yet')
+  const [lastGuessTime, setLastGuessTime] = useState(new Date())
 
   let myGuessString = 'You have not guessed yet'
-  if (myGuess != 'No guess yet') myGuessString = 'You guessed '+myGuess
+  if (myGuess != 'No guess yet') 
+      myGuessString = 'You guessed '+myGuess
+
+  let computerGuessString = 'Computer has not guessed yet'
+  if (computerGuess != 'No guess yet') 
+      computerGuessString = 'Computer guessed '+computerGuess
+
+  let result = "It was a tie"
+  if (myGuess != 'No guess yet' || computerGuess == 'No guess yet') 
+      computerGuessString = 'Computer Guess '+computerGuess
+
+  if (
+    (myGuess == 'rock' && computerGuess == 'scissors') ||
+    (myGuess == 'paper' && computerGuess == 'rock') ||
+    (myGuess == 'scissors' && computerGuess == 'paper')
+  ) result = "You won!"
+
+  if (
+    (myGuess == 'rock' && computerGuess == 'paper') ||
+    (myGuess == 'paper' && computerGuess == 'scissors') ||
+    (myGuess == 'scissors' && computerGuess == 'rock')
+  ) result = "The Computer won!"
 
   useEffect(()=>{
-    console.log('You made a new choice of', myGuess)
-  }, [myGuess])
+    if (myGuess == 'No guess yet') return;
+
+    async function getComputerGuess(){
+      const result = await fetch(computerOpponentUrl)
+      const guess = await result.text()
+      setComputerGuess(guess)
+    }
+
+    getComputerGuess()    
+  }, [myGuess, lastGuessTime])
+
+  function setGuess(guess) {
+    setMyGuess(guess)
+    setLastGuessTime(new Date())
+  }
 
   return (
     <div>          
       <h1>Welcome to Rock Paper Scissors</h1>
       <div>{myGuessString}</div>
+      <div>{computerGuessString}</div>
+      <div>{result}</div>
       <br />
-      <Button text="Rock" fire={()=>{setMyGuess('rock')}}/>
-      <Button text="Paper" fire={()=>{setMyGuess('paper')}}/>
-      <Button text="Scissors" fire={()=>{setMyGuess('scissors')}}/>
+      <Button text="Rock" fire={()=>{setGuess('rock')}}/>
+      <Button text="Paper" fire={()=>{setGuess('paper')}}/>
+      <Button text="Scissors" fire={()=>{setGuess('scissors')}}/>
     </div>
   )
 }
