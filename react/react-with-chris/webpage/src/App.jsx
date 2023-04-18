62915c901c335b156501941cabc4d4e181ca02bc
kenneth-alt <kenneth.ugo15@gmail.com>
2023-04-18 16:10:35 -0600

    first commit

--- react/react-with-chris/webpage/src/App.jsx
+++ react/react-with-chris/webpage/src/App.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 const computerOpponentUrl = 'http://localhost:5051/rps'
