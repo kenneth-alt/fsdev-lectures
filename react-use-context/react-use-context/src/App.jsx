1afcd4f451bb730106cd864ed805b5c55124c866
kenneth-alt <kenneth.ugo15@gmail.com>
2023-04-21 10:46:03 -0600

    Last working checkpoint

--- react-use-context/react-use-context/src/App.jsx
+++ react-use-context/react-use-context/src/App.jsx
@@ -2,18 +2,23 @@ import { useState } from 'react'
 import './App.css'
 import MainPage from './components/MainPage';
 import Footer from './components/Footer';
+import LoginScreen from './components/LoginScreen';
 
 function App() {
-  const [user, setUser] = useState('Ken')
+  const [user, setUser] = useState(null)
 
   return (
     <div style={{
       width: '100%', height: '100%', position: 'fixed', top: 0, left: 0, display: 'flex', alignItems: 'center', justifyContent: 'grow', flexDirection: 'column'
       }}>
-        <div style={{ flexGrow: 1}}>
-          <MainPage user={user} />
-        </div>
-      <Footer user={user} setUser={setUser} />
+        <div style={{ flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          {user != null ? 
+            <MainPage user={user} /> :
+            <LoginScreen setUser={setUser} /> 
+          }
+          </div>
+          <Footer user={user} setUser={setUser} />
+          
     </div>
   )
 }
