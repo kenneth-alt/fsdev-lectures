09381b71643ef16dda5112ce8e66a7dadae4b27d
kenneth-alt <kenneth.ugo15@gmail.com>
2023-05-12 09:53:30 -0600

    configure server

--- react/react-fullstack-review/server/server.js
+++ react/react-fullstack-review/server/server.js
@@ -3,7 +3,9 @@ import express from 'express'
 const app = express()
 const port = 2012
 
-app.use('/', (req, res) => {
+app.use(express.json())
+
+app.use('/api', (req, res) => {
     res.send('Hello world');
   });
 
