16530b1a695ead8f10b390000604444e8ebe768c
kenneth-alt <kenneth.ugo15@gmail.com>
2023-05-12 09:22:23 -0600

    last commit

---
+++ react/functions_review/functions.js
@@ -0,0 +1,7 @@
+const squareFn = (num) => {
+    const numSquared = num * num
+    return(numSquared)
+}
+
+
+console.log(squareFn(45))
---
+++ react/react_review/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [helloWorld, setHelloWorld] = useState("Hello World")
+
+    console.log(helloWorld)
+
+
+  return (
+    <>
+     
+     <
+
+        
+    </>
+  )
+}
+
+export default App
